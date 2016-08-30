e2b5ab75c1d5b0c5827689929f70c626045d77db
LuxXx <pr0v0ziert@gmx.de>
2016-08-30 14:05:42 +0200

    added keygen for level 1

---
+++ borismilner/level-1/solution/keygen/LevelOne.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class LevelOne {
+	public static void main(String[] args) {
+		String username;
+		if (args.length > 0) {
+			username = args[0];
+		} else {
+			System.out.println("Enter username:");
+			Scanner s = new Scanner(System.in);
+			username = s.nextLine();
+			s.close();
+		}
+		System.out.println("Key: " + generateKey(username));
+	}
+	public static int generateKey(String username) {
+		int key = 0;
+		for (int i = 0; i < username.length(); i++) {
+			key += (int) username.charAt(i);
+		}
+		return key;
+	}
+}
